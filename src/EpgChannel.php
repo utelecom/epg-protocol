74ca5385d545253423395e8a8b4406ca45b88ada
toxxxa <toxxxa@gmail.com>
2021-11-16 18:50:38 +0200

    new: Добавляет методы EpgChannel::getTitle(), EpgChannel::getTranslation()

--- src/EpgChannel.php
+++ src/EpgChannel.php
@@ -33,4 +33,34 @@ class EpgChannel
     public bool $isAdult;
 
     public bool $isEnabled;
+
+    public function getTitle(?string $lang = null): ?string
+    {
+        $translation = $this->getTranslation($lang);
+        if (null === $translation) {
+            return null;
+        }
+
+        return $translation->title;
+    }
+
+    private function getTranslation(?string $lang = null): ?EpgChannelTranslation
+    {
+        if (empty($this->translations)) {
+            return null;
+        }
+
+        if (null === $lang) {
+            $firstKey = array_key_first($this->translations);
+            return $this->translations[$firstKey];
+        }
+
+        foreach ($this->translations as $translation) {
+            if ($lang === $translation->lang) {
+                return $translation;
+            }
+        }
+
+        return null;
+    }
 }
